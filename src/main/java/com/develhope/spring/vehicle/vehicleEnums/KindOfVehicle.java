33be4667c5ae54e634329a9c6418dd19d6dda19a
Gabriel <g.cellardoor.delloiacovo@gmail.com>
2024-05-22 11:16:25 +0200

    add converterFromString input

--- src/main/java/com/develhope/spring/vehicle/vehicleEnums/KindOfVehicle.java
+++ src/main/java/com/develhope/spring/vehicle/vehicleEnums/KindOfVehicle.java
@@ -5,5 +5,17 @@ public enum KindOfVehicle {
     MOTO,
     SCOOTER,
     VAN,
-    TRUCK
+    TRUCK,
+    NOT_DEFINED
+
+    public static KindOfVehicle convertFromString(String type) {
+        return switch (type.toLowerCase()) {
+            case "car" -> KindOfVehicle.CAR;
+            case "motorbike" -> KindOfVehicle.MOTO;
+            case "scooter" -> KindOfVehicle.SCOOTER;
+            case "van" -> KindOfVehicle.VAN;
+            case "truck" -> KindOfVehicle.TRUCK;
+            default -> KindOfVehicle.NOT_DEFINED;
+        };
+    }
 }
