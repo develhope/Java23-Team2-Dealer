8e9ea44a8305946d7c02215cb7bfd6aeacc69679
AleDev <aledev@example.com>
2024-05-24 17:22:31 +0200

    fix constructor and getter

--- src/main/java/com/develhope/spring/model/user/Rental.java
+++ src/main/java/com/develhope/spring/model/user/Rental.java
@@ -1,14 +1,23 @@
 package com.develhope.spring.model.user;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.time.LocalDate;
 
+@Entity
 public class Rental {
+    @Column(nullable = false)
     private LocalDate startDate;
+    @Column(nullable = false)
     private LocalDate endDate;
+    @Column(nullable = false)
     private BigDecimal dailyCost;
+    @Column(nullable = false)
     private BigDecimal totalCost;
+    @Column(nullable = false)
     private boolean paid;
 
     //it was decided to use vehicle (object) but instead of using
@@ -45,6 +54,52 @@ public class Rental {
         return paid;
     }
 
+    public Rental(LocalDate startDate, LocalDate endDate, BigDecimal dailyCost, BigDecimal totalCost, boolean paid, int vehicleId) {
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.dailyCost = dailyCost;
+        this.totalCost = totalCost;
+        this.paid = paid;
+        this.vehicleId = vehicleId;
+    }
 
+    public Rental() {
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public BigDecimal getDailyCost() {
+        return dailyCost;
+    }
+
+    public void setDailyCost(BigDecimal dailyCost) {
+        this.dailyCost = dailyCost;
+    }
+
+    public void setTotalCost(BigDecimal totalCost) {
+        this.totalCost = totalCost;
+    }
+
+    public int getVehicleId() {
+        return vehicleId;
+    }
+
+    public void setVehicleId(int vehicleId) {
+        this.vehicleId = vehicleId;
+    }
 }
 
