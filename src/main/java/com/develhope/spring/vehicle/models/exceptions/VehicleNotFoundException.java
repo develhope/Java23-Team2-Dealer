bf97de9ed6d6b469a7b242adcabf87add35c4af4
Calyen93 <genco.claudio3@gmail.com>
2024-05-24 10:51:51 +0200

    Admin delete vehicle method and exceptions

---
+++ src/main/java/com/develhope/spring/vehicle/models/exceptions/VehicleNotFoundException.java
@@ -0,0 +1,7 @@
+package com.develhope.spring.vehicle.models.exceptions;
+
+public class VehicleNotFoundException extends Exception {
+    public VehicleNotFoundException(String message) {
+        super(message);
+    }
+}
