03c361379601278fd4ac20303036835eb171c6e1
Sergio <sergio.hanganu@gmail.com>
2024-06-11 17:17:11 +0200

    Changes

--- src/main/java/com/develhope/spring/vehicles/vehicleOptionals/models/Rims.java
+++ src/main/java/com/develhope/spring/vehicles/vehicleOptionals/models/Rims.java
@@ -1,6 +1,5 @@
 package com.develhope.spring.vehicles.vehicleOptionals.models;
-
-import aj.org.objectweb.asm.Opcodes;
+import com.develhope.spring.vehicles.vehicleOptionals.enums.RimType;
 import jakarta.persistence.*;
 import org.springframework.data.util.Optionals;
 
@@ -15,12 +14,23 @@ public class Rims implements Optionals {
     private long id;
     @Column(nullable = false)
     private BigDecimal price;
+    private RimType rimType;
 
     public Rims(){}
 
-    public Rims(long id, BigDecimal price) {
+    public Rims(long id, BigDecimal price , RimType rimType) {
         this.id = id;
         this.price = price.setScale(2 , RoundingMode.HALF_EVEN);
+        this.rimType = rimType;
+
+    }
+
+    public RimType getRimType() {
+        return rimType;
+    }
+
+    public void setRimType(RimType rimType) {
+        this.rimType = rimType;
     }
 
     public long getId() {
