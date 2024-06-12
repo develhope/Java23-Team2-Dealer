580fcf95e09320f0698117af2e1a861e001a8797
Calyen93 <genco.claudio3@gmail.com>
2024-06-12 15:19:02 +0200

    Refactor OrderControllerTest to OrderIntegrationTest

--- src/test/java/com/develhope/spring/deals/controllers/OrderIntegrationTest.java
+++ src/test/java/com/develhope/spring/deals/controllers/OrderIntegrationTest.java
@@ -14,7 +14,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
-public class OrderControllerTest {
+public class OrderIntegrationTest {
 
     @Autowired
     private MockMvc mockMvc;
