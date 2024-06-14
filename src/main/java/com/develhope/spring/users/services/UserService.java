14d69e33f1523edc5c8a750b6b6091d55f9a2b6f
Calyen93 <genco.claudio3@gmail.com>
2024-06-14 11:59:36 +0200

    Added Logger to UserService & UserRegistration

--- src/main/java/com/develhope/spring/users/services/UserService.java
+++ src/main/java/com/develhope/spring/users/services/UserService.java
@@ -2,10 +2,13 @@ package com.develhope.spring.users.services;
 
 import com.develhope.spring.exceptions.UserAlreadyExistException;
 import com.develhope.spring.users.components.UserMapper;
+import com.develhope.spring.users.controllers.RegistrationController;
 import com.develhope.spring.users.dtos.UserRegistrationDTO;
 import com.develhope.spring.users.dtos.UserSavedDTO;
 import com.develhope.spring.users.models.User;
 import com.develhope.spring.users.repositories.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,6 +21,8 @@ public class UserService implements IUserService {
     @Autowired
     private UserMapper userMapper;
 
+    private final Logger logger = LoggerFactory.getLogger(UserService.class);
+
     public User getBy(Long id) {
         return userRepository.findById(id).orElseThrow();
     }
@@ -25,11 +30,13 @@ public class UserService implements IUserService {
     @Override
     public UserSavedDTO registerNewUserAccount(UserRegistrationDTO userRegistrationDTO) {
         if (emailExists(userRegistrationDTO.getEmail())) {
+            logger.warn("Email già esistente " + userRegistrationDTO.getEmail());
             throw new UserAlreadyExistException("There is an account with that email address: "
                     + userRegistrationDTO.getEmail());
         }
         User user = userMapper.toEntity(userRegistrationDTO);
         User savedUser = userRepository.save(user);
+        logger.info(savedUser + " Registrato");
         return userMapper.toUserSavedDTO(savedUser);
     }
 
