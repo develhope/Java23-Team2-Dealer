1ed66a6ed9005e1d199b2d66fe685d0f3073e6d6
Gabriel <g.cellardoor.delloiacovo@gmail.com>
2024-06-19 22:47:54 +0200

    add dto without OR

--- src/main/java/com/develhope/spring/vehicles/dtos/VehicleFilterDTO.java
+++ src/main/java/com/develhope/spring/vehicles/dtos/VehicleFilterDTO.java
@@ -1,60 +1,56 @@
-//package com.develhope.spring.vehicles.dtos;
-//
-//public class VehicleFilterDTO {
-//
-//    private String vehicleType;
-//
-//    private String brand;
-//
-//    private String displacement;
-//
-//    private String color;
-//
-//    private String power;
-//
-//    private String gear;
-//
-//    private String year;
-//
-//    private String powerSupply;
-//
-//    private String priceOver;
-//
-//    private String priceBetween;
-//
-//    private String priceBelow;
-//
-//    private String usedFlag;
-//
-//    private String discountedFlag;
-//
-//    public VehicleFilterDTO(
-//            String vehicleType,
-//            String brand,
-//            String displacement,
-//            String color,
-//            String power,
-//            String gear,
-//            String year,
-//            String powerSupply,
-//            String priceOver,
-//            String priceBetween,
-//            String priceBelow,
-//            String usedFlag,
-//            String discountedFlag
-//    ) {
-//        this.vehicleType = "vehicleType:" + vehicleType;
-//        this.brand = "brand:" + brand;
-//        this.displacement = "displacement:" + displacement;
-//        this.color = "color:" + color;
-//        this.power = "power:" + power;
-//        this.gear = "gear:" + gear;
-//        this.year = "registrationYear:" + year;
-//        this.powerSupply = "powerSupply:" + powerSupply;
-//        this.priceOver = "price>" + priceOver;
-//        this.priceBetween = "pricepriceBetween;
-//        this.priceBelow = priceBelow;
-//        this.usedFlag = usedFlag;
-//        this.discountedFlag = discountedFlag;
-//    }
-//}
+package com.develhope.spring.vehicles.dtos;
+
+public class VehicleFilterDTO {
+
+    private String vehicleType;
+
+    private String brand;
+
+    private String displacement;
+
+    private String color;
+
+    private String power;
+
+    private String gear;
+
+    private String year;
+
+    private String powerSupply;
+
+    private String priceOver;
+
+    private String priceBelow;
+
+    private String usedFlag;
+
+    private String discountedFlag;
+
+    public VehicleFilterDTO(
+            String vehicleType,
+            String brand,
+            String displacement,
+            String color,
+            String power,
+            String gear,
+            String year,
+            String powerSupply,
+            String priceOver,
+            String priceBelow,
+            String usedFlag,
+            String discountedFlag
+    ) {
+        this.vehicleType = "vehicleType:" + vehicleType;
+        this.brand = "brand:" + brand;
+        this.displacement = "displacement:" + displacement;
+        this.color = "color:" + color;
+        this.power = "power:" + power;
+        this.gear = "gear:" + gear;
+        this.year = "registrationYear:" + year;
+        this.powerSupply = "powerSupply:" + powerSupply;
+        this.priceOver = "price>" + priceOver;
+        this.priceBelow = "price<" + priceBelow;
+        this.usedFlag = "usedFlag:" + usedFlag;
+        this.discountedFlag = "discountedFlag:" + discountedFlag;
+    }
+}
